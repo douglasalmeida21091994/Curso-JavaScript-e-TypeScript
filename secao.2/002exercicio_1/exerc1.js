50e86aeb4ce69000c003d3f451b6be7217ad90d9
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-05-24 23:25:08 -0300

    002exercicio_1

---
+++ secao.2/002exercicio_1/exerc1.js
@@ -0,0 +1,7 @@
+// PRIMEIRO EXERCÍCIO COM JS
+
+console.log('Meu nome é "Douglas Almeida". Estou aprendendo JavaScript às', 10, 'da noite'); // dessa forma, pois quero que o número seja Number e não String
+
+console.log('Meu noma é "Douglas Almeida". Estou aprendendo JS às 10 da noite'); // Dessa forma fica tudo string
+
+// Number dentro de String, deverá ser separado por vírgula
