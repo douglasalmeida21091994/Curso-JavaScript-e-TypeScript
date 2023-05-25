e427e253de3c63949b6859148681c4c68a1090ff
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-05-25 00:22:51 -0300

    Function Alert And HTML5 And JS

--- secao.2/006aula/index.js
+++ secao.2/006aula/index.js
@@ -2,4 +2,17 @@
 
 /* criar os arquivos separadamente */
 
-console.log('Colocando este arquivo dentro do meu HTML5')
+
+console.log('Colocando este arquivo dentro do meu HTML5');
+
+console.log("My name is Douglas!");
+alert("Atualização...");
+alert('Nova atualização em 5s');
+
+
+// LEMBRANDO QUE O ; NO FINAL DO JS É FACULTATIVO
+
+
+// essa função mostrará o alerta na tela do usuário, que deverá clicar em "OK" para continuar e mostrar meu front (HTML5) e/ou meu console.
+
+//A função ALERT não funciona no NODE
