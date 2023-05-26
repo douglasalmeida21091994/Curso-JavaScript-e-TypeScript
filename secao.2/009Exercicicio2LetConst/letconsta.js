c63f9f0a0788a82e1212b9077238013c6d85fd88
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-05-25 23:37:31 -0300

    CONST e LET

--- secao.2/009Exercicicio2LetConst/letconsta.js
+++ secao.2/009Exercicicio2LetConst/letconsta.js
@@ -22,6 +22,7 @@ console.log (nome, sobreNome, "tem", idade, descricao1, ". Seu nome é:", nomeFi
 const nome2 = 'Douglas F. Mota';
 const sobreNome2 = " de Almeida,";
 const idade2 = ` 28 anos,`;
+const idade4 = ` 28 anos`;
 let idade3 = 28;
 let peso = 90;
 const alturaEmM = 1.71;
@@ -31,14 +32,14 @@ const alturaEmM = 1.71;
 let imc = peso / (alturaEmM * alturaEmM);  // IMC (Índice de Massa Corporal) é calculado: PESO / (ALTURA * ALTURA)
 
 
-const geral = nome2 + sobreNome2 + idade2 + " tem " + peso + " Kg e " + alturaEmM + " de altura.";
+const geral = nome2 + sobreNome2 + idade2 + " tem " + peso + " Kg e " + alturaEmM + " de altura.";  // CONCATERNANDO
 
 
 /* OU PODERÁ SER DIRETIO NO LOG, sem definifir uma constante com tudo e ficaria: */
 
 console.log(nome2, sobreNome2, idade2, " tem ", peso, " Kg e ", alturaEmM, " de altura... FEITO DIRETO NO CONSOLE.LOG")
 
-console.log("Resultado o meu IMC: ", imc) ;
+console.log("Resultado o meu IMC: ", imc);
 console.log(geral, " FEITO PEGAR PELA MINHA CONST GERAL...");
 
 
@@ -51,6 +52,16 @@ anoNascimento = 2023 - idade3; // declarei idade3, pois tem que ser Number, e a 
 console.log(anoNascimento);
 
 
+// UNINDO OS VALORES e tornando apenas uma STRING:  ' ' = espaçamento  || + = e som de i
+
+console.log(nome2 + ' ' + sobreNome2 + ' ' + idade4 + '.');  // mais complicado de ler
 
 
+// Usando em: TEMPLATE STRING:  || FORMA MAIS USADA que é sifrão e chaves para ler a variável ou constante, no JS
 
+console.log("USANDO O MÉTODO: Template String:")
+console.log(`meu nome é ${nome2} e tenho ${idade3}.`) // mais fácil de ler
+
+console.log (`Meu IMC é: ${imc}.`)
+
+console.log(`${nome2} ${sobreNome2} ${idade3} anos, tem ${peso}Kg e ${alturaEmM}m de altura.`)
