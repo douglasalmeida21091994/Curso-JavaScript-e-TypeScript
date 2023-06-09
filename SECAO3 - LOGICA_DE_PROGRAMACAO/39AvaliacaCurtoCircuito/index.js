8b13978a3ba41e0bd51b565cee214b6c0269cf34
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-06-09 00:22:48 -0300

    39. Avaliação de Curto-Circuito (Short-Circuit)
    39. Avaliação de Curto-Circuito (Short-Circuit)

--- SECAO3 - LOGICA_DE_PROGRAMACAO/39AvaliacaCurtoCircuito/index.js
+++ SECAO3 - LOGICA_DE_PROGRAMACAO/39AvaliacaCurtoCircuito/index.js
@@ -1,7 +1,5 @@
 /* 
 
-OPERADORES LÓGICOS:
-
 && -> AND que em português = E      // TODAS AS EXPRESSÕES PRECISAM SER VERDADEIRAS PARA RETORNAR TRUE
 || -> OR que em português = OU      // APENAS UMA EXPRESSÃO PRECISA SER VERDADEIRA PARA RETORNAR TRUE
 !  -> NOT que em português = NÃO    // ELA INVERTE UMA EXPRESSÃO OU NEGA UMA EXPRESSÃO // FALSE PARA A SER TRUE e TRUE PARA A SER FALSE
@@ -13,3 +11,68 @@ OPERADORES LÓGICOS:
 console.log("Douglas" && false && "Almeida"); // String false String => Retonará: false
 console.log("Douglas" && true && "Almeida"); // String true String => Retonará: Almeida, POIS TODAS SÃO TRUE
 console.log("Douglas" && true && NaN); // Retorna: NaN
+
+
+/*
+
+QUALQUER COISA DIFERENTE DO QUE ESTÁ ABAIXO, É AVALIADO PELO JS, COMO TRUE
+
+false
+0
+'' ou "" ou ``  STRINGS VAZIAS
+numm
+undefined
+NaN
+
+Esses acima são todos FALSE / FALSY
+
+*/
+
+console.log("Douglas" && "Paola"); // RESULTADO: PAOLO, pois ambos são true || RETORNA SEMPRE O ÚLTIMO EM CASOS DE TRUE
+
+console.log("Douglas" && null && "Paola"); // Retorna: Null
+console.log("Douglas" && "" && "Paola"); // Retorna: vazio (uma string vazia)
+console.log("Douglas" && undefined && "Paola"); // Retorna: undefined
+console.log("Douglas" && false && "Paola"); // Retorna: false
+
+
+function falaOi(){
+    return "Oi";
+}
+
+const vaiExecutar = false; // ou poderia ser qualquer outro valor que retorna FALSE, conforme coloquei acima.
+
+console.log(vaiExecutar && falaOi()); // Nesse caso eu chequei e só executo se a minha variável for TRUE. No caso, como é FALSE, retorna False e NÃO executará minha function
+
+
+
+function falaOla(){
+    return "Olá";
+}
+
+const executando = true; // Poderia colocar uma string. Exemplo: "DOUGLAS FRANCISCO"
+
+console.log(executando && falaOla()); // EXECUTOU MINHA FUNÇÃO, POIS MINHA CONST É TRUE
+
+// como a minha função está na última posição e ambas são TRUE, vai executar apenas a function
+
+
+/*                 USANDO O OR: ||                  */
+
+console.log(0 || false || "Douglas" || null || "Almeida" || true); // 0 = false / FALSE / VERDADEIRO / FALSE / VERDADEIRO / VERDADEIRO
+
+//  imprimará o primeiro TRUE = "Douglas"
+
+const corUsuario = null; 
+const corPadrao = corUsuario || "black";
+console.log(corPadrao); // Resultado: "black" , pois o usuário não selecionou a cor
+
+
+const a = 0;
+const b = null;
+const c = 'False'; // Só essa é verdadeira
+const d = false;
+const e = undefined;
+const f = NaN;
+
+console.log(a || b || c || d || e || f);  // imprimirá a const c
