8ec47ed70cd10134944f1f4f565a85bcf6bb9eaa
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-06-17 22:15:31 -0300

    58. While e Do While - Estrutura de repetição

--- SECAO3 - LOGICA_DE_PROGRAMACAO/58_While_DoWhile/index.js
+++ SECAO3 - LOGICA_DE_PROGRAMACAO/58_While_DoWhile/index.js
@@ -1,3 +1,40 @@
 
 // 58. While e Do While - Estrutura de repetição
 
+
+// While, checa a condição e depois executa o laço
+
+const nome = "Douglas Francisco";
+let i = 0; // é onde inicia
+
+while (i < nome.length) {
+    console.log(nome[i]);
+    i++; // nunca esquecer de colocar esse incremento para atualizar minha variável, senão vai entrar no loop infinito
+}
+
+// FUNÇÃO QUE GERA UM NÚMERO ALEATÓRIO
+
+function random(min, max) {
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+//let rand = random(min, max);
+let rand = 10;
+
+// !== siginifica DIFERENTE
+
+while  (rand !== 10) {  // Se o rand for diferente de 10, continua executande, do contrário sairá fora do laço
+    rand = random(min, max);
+    console.log(rand);
+}
+
+///////////////////// do while:  SEMPRE executa primeiro para depois checar
+
+
+do {
+    rand = random(min, max);
+    console.log(rand);
+} while(rand !== 10);
