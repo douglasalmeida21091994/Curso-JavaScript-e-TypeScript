9a26a92dc63ceb38db3650495c6ea14fbf768878
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-06-18 23:22:18 -0300

    65_setInterval_e_setTimeout_ATUALIZAR_HORA_AUTOMATICAMENTE
    65_setInterval_e_setTimeout_ATUALIZAR_HORA_AUTOMATICAMENTE

--- SECAO3 - LOGICA_DE_PROGRAMACAO/65_setInterval_e_setTimeout_ATUALIZAR_HORA_AUTOMATICAMENTE/index.js
+++ SECAO3 - LOGICA_DE_PROGRAMACAO/65_setInterval_e_setTimeout_ATUALIZAR_HORA_AUTOMATICAMENTE/index.js
@@ -12,9 +12,25 @@ function funcaodoInterval() {
     console.log(mostraHora());
 }
 
-setInterval (function() {
+
+/* setInterval(function() {
     console.log(mostraHora());
-}, 1000);
+}, 1000); */
+
+///  COLOCANDO O setInterval dentro de uma VARIÁVEL, para usar o setTimeout:
+
+const time = setInterval(function() {
+    console.log(mostraHora());
+}, 1000); // 1 segundo
 
 
-///  
+// Informando em quando tempo vai parar o código acima:
+
+setTimeout(function() {
+    clearInterval(time);
+    console.log("Teste"); // posso deixar apenas esse console, para exibir exatamente ao final da constagem
+}, 5000); // 5 segundos
+
+setTimeout(function() {
+    console.log("Exibindo texto após os 5 segundos acima.")
+}, 6000); // 6 segundos
