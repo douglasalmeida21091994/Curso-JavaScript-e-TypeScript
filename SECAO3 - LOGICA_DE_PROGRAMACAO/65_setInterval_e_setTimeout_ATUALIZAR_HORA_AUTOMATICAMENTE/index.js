4b60836d8c5644d166d604281caa91d181599136
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2023-06-18 23:13:03 -0300

    65_setInterval_e_setTimeout_ATUALIZAR_HORA_AUTOMATICAMENTE
    65_setInterval_e_setTimeout_ATUALIZAR_HORA_AUTOMATICAMENTE

--- SECAO3 - LOGICA_DE_PROGRAMACAO/65_setInterval_e_setTimeout_ATUALIZAR_HORA_AUTOMATICAMENTE/index.js
+++ SECAO3 - LOGICA_DE_PROGRAMACAO/65_setInterval_e_setTimeout_ATUALIZAR_HORA_AUTOMATICAMENTE/index.js
@@ -2,10 +2,19 @@
 
 // 65. setInterval e setTimeout
 
-function myHora() {
+function mostraHora() {
     let data = new Date();
 
     return data.toLocaleTimeString("pt-BR", {hour12: false}); // apenas retornando o valor, não exibirá a hora, tem que dár um console.log()
 }
 
-console.log(myHora());
+function funcaodoInterval() {
+    console.log(mostraHora());
+}
+
+setInterval (function() {
+    console.log(mostraHora());
+}, 1000);
+
+
+///  
